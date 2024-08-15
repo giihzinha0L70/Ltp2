13d5b693b5e567c6db68a4a09c04582ca93fdc38
giihzinha0L70 <gihgomes1603@gmail.com>
2024-08-15 16:57:52 -0300

    Add files via upload

--- ModeloHeranca2_2024/src/ModeloHeranca_2024/Testa.java
+++ ModeloHeranca2_2024/src/ModeloHeranca_2024/Testa.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package ModeloHeranca_2024;
 
 /**
@@ -15,7 +11,21 @@ public class Testa {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        DeSal DS = new DeSal("10.10.24", "02.08.26", 6378517, "20min", 3, 30);
+        System.out.println(DS);
+        
+        Recheado R = new Recheado("20.08.25", "25.08.25", 6958234, "15min", "pão doce", "creme de leite");
+        System.out.println(R);
+        
+        Bolo B = new Bolo("04.02.24", "08.02.24", 6666667, "bolo de festa", 15, 257);
+        System.out.println(B);
+        
+        Assado A = new Assado("16.03.24", "17.03.24",  1236547, "de presunto e queijo", 5, 20);
+        System.out.println(A);
+        
+        Frito F = new Frito("16.03.24", "17.03.24",  1236548, "enroladinho de salsicha", 33, "farinha de arroz");
+        System.out.println(F);
     }
     
 }
+                        
